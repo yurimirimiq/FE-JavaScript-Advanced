a1dad7c2fab8af984787a4da392e43a429acd5e4
yerroong <wbflqldks90@inha.edu>
2025-04-03 15:28:00 +0900

    6번 과제 수정

--- 6_solution.js
+++ 6_solution.js
@@ -4,7 +4,6 @@ const arr = [
     { part: "fe", name: "수진", age: 22 },
     { part: "fe", name: "병윤", age: 23 },
     { part: "fe", name: "보연", age: 21 },
-    { part: "staff", name: "수빈", age: 24 }
-];
-
-//코드 작성
+    { part: "staff", name: "수빈", age: 24 },
+    { part: "be", name: "기현", age: 25 }
+  ]
