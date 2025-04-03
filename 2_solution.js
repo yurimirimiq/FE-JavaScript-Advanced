ad6244cf5e83543772b3d701a6eea32e63e61307
강유림 <yurimirimiq@gmail.com>
2025-04-03 20:06:20 +0900

    과제

--- 2_solution.js
+++ 2_solution.js
@@ -6,6 +6,7 @@ let user = {
 };
 
 // 여기에 코드를 작성해주세요 sjandjfuqekd
+const user = [Name: "민서", age : 21, isAdmin : false]
 
 alert(Name); // "민서"
 alert(age); // 21
--- 6_solution.js
+++ 6_solution.js
@@ -6,19 +6,3 @@ let arr = [
     { part: "be", name: "기현", age: 25 }
   ];
   
-  // 1. staff 제거
-  arr = arr.filter(member => member.part !== "staff");
-  
-  // 2. "fe"는 남기되 "보연"은 제거
-  arr = arr.filter(member => !(member.part === "fe" && member.name === "보연"));
-  
-  // 3. 나이 내림차순 정렬
-  arr.sort((a, b) => b.age - a.age);
-  
-  // 4. 이름만 뽑아 배열 만들기
-  const names = arr.map(member => member.name);
-  console.log("최종 이름 배열:", names);
-  
-  // 5. 이름 한 줄씩 출력
-  names.forEach(name => console.log(name));
-  
