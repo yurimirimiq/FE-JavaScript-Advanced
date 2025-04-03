ed5464a492e9d25114185a961d396ebe93524208
강유림 <yurimirimiq@gmail.com>
2025-04-03 19:46:42 +0900

    1번 퀴즈

--- 1_solution.js
+++ 1_solution.js
@@ -4,7 +4,7 @@ let name = "신석";
 
 // ? 부분에 예측 값을 작성해주세요(코드 실행 금지)
 
-alert(`hello ${1}`); // ?
+alert(`hello ${1}`); // ? ???
 alert(`hello ${"name"}`); // ?
 alert("hello ${name}"); // ?
 alert(`hello ${name}`); // ?
